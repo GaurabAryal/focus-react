b627d58540260a9bb67927a8cb17a8cf66b49bac
jeremyukim <jeremyukim@gmail.com>
2016-12-16 14:42:05 -0500

    Set up state array of todo entries

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -6,24 +6,50 @@ import FontAwesome from 'react-fontawesome';
 import TodoEntry from './TodoEntry.js';
 
 class Todo extends React.Component {
-  // state array keeps track of strings for each todo, render TodoEntry for each one
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    let newTodo = document.getElementById('newTodo');
+    newTodo.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+          this.addNewTodo();
+      }
+    });
+  }
+
+  addNewTodo() {
+    let newTodo = document.getElementById('newTodo').value;
+    console.log('newTodo', newTodo);
+    let tempTodos = this.state.todos;
+    tempTodos.push(newTodo);
+    this.setState({todos: tempTodos});
+    console.log('todos: ', this.state.todos);
+    document.getElementById('newTodo').value = '';
+  }
+
+  renderTodos() {
+    // state array keeps track of strings for each todo, render TodoEntry for each one
+  }
+
   render() {
     return (
       <div>
         <div className='row'>
           <div className='col-lg-8 col-xl-6'>
             <h2 className={cx(styles.promptLight, styles.title)}>Tasks for Today</h2>
-            <input type='text' placeholder='What needs to be done?' className={styles.textField}/>
+            <input id='newTodo' type='text' placeholder='What needs to be done?' className={styles.textField}/>
           </div>
         </div>
         <div className='row'>
           <div className={styles.containTodo}>
             {this.renderTodos}
             {/* temporary direct render for testing UI */}
-            <TodoEntry/>
-            <TodoEntry/>
-            <TodoEntry/>
-            <TodoEntry/>
             <TodoEntry/>
             <TodoEntry/>
             <TodoEntry/>
